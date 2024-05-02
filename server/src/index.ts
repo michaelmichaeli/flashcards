54b107d2539572aa5b22ff11f87322d08303ff4a
michaelmichaeli888 <michaelmichaeli888@gmail.com>
2024-05-02 11:31:02 +0300

    feat: add dotenv to secure the app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,27 @@
-import express from "express";
-const app = express();
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-	res.send("Hello World");
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import Deck from "./models/Deck";
+
+const PORT = 5000;
+
+const app = express();
+app.use(express.json());
+
+app.post("/decks", async (req: Request, res: Response) => {
+	console.log(req.body);
+
+	const newDeck = new Deck({
+		title: req.body.title,
+		name: req.body.name,
+	});
+	const createdDeck = await newDeck.save();
+	res.json(createdDeck);
 });
 
-app.listen(5000);
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+	console.log("listening to port ", PORT);
+});
+
+app.listen(PORT);
